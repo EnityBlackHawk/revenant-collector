5938f753cfe56826c9bdd42213e155a17fe8d0e6
EnityBlackHawk <luanf2003@gmail.com>
2025-11-04 19:32:30 -0300

    feat: Add signal handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-
 mod stack_entry;
 mod table;
 
+use libc::{signal, c_int};
 use backtrace::Backtrace;
 use stack_entry::StackEntry;
 use crate::table::print_table;
@@ -16,7 +16,30 @@ pub extern "C" fn print_stacktrace() {
             entries.push( StackEntry::from_backtrace_symbol(symbol) );
         }
     }
-
     print_table(entries.as_slice());
+}
 
+extern "C" fn handler(sig: c_int) {
+    eprintln!("Caught signal: {}", sig);
+    print_stacktrace();
+    std::process::exit(1);
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn setup_one_signal_handler(sig : c_int) -> bool {
+    unsafe {
+        if signal(sig, handler as usize) == libc::SIG_ERR {
+            eprintln!("Error setting up signal handler for SIGSEGV");
+            return false
+        }
+        true
+    }
+}
+
+pub fn setup_signal_handlers(signals : Vec<c_int>) {
+    for &sig in signals.iter() {
+        if !setup_one_signal_handler(sig) {
+            eprintln!("Failed to set up handler for signal: {}", sig);
+        }
+    }
 }
